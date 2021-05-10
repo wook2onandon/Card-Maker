32fd206df232f0a2824a1e3edfba6690d9593a9a
wook2onandon <babyldk@gmail.com>
2021-05-11 02:42:32 +0900

    add card and edit form component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import Editor from '../editor/editor';
 import Footer from '../footer/footer';
@@ -7,6 +7,41 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Jongwook',
+      company: 'Samsung',
+      theme: 'dark',
+      title: 'Frontend Developer',
+      email: 'babyldk@gmail.com',
+      message: 'go for it',
+      fileName: 'wook',
+      fileURL: null,
+    },
+    {
+      id: '2',
+      name: 'Taehoon',
+      company: 'Apple',
+      theme: 'light',
+      title: 'Frontend Developer',
+      email: 'sagukorea@gmail.com',
+      message: 'go for it',
+      fileName: 'hoon',
+      fileURL: null,
+    },
+    {
+      id: '3',
+      name: 'Joowon',
+      company: 'Hyundai',
+      theme: 'colorful',
+      title: 'Video Editor',
+      email: 'dangun@gmail.com',
+      message: 'go for it',
+      fileName: 'won',
+      fileURL: null,
+    },
+  ]);
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -23,8 +58,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
